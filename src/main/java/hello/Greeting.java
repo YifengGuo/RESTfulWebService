cb63fc4baed5c2025be0db23b0d1bc887b53a4d4
YifengGuo <gyf940517@gmail.com>
2018-07-15 17:42:36 -0400

    first commit

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,23 @@
+package hello;
+
+/**
+ * @author: yifeng
+ */
+
+public class Greeting {
+    private long id;
+    private String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
